33ca0ca957995c37c030440c1de3d56f10586144
Rushabh <rushabhnagpure864@gmail.com>
2023-03-02 18:06:57 +0530

    new key word is missing in it and code is done properly

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -13,8 +13,8 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-            super(emailId ,  Integer.MAX_VALUE);
-            this.calendar = ArrayList<>();
+            super(emailId, Integer.MAX_VALUE);
+            this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
@@ -24,27 +24,22 @@ public class Workspace extends Gmail{
     }
 
     public int findMaxMeetings(){
-        // find the maximum number of meetings you can attend
-        // 1. At a particular time, you can be present in at most one meeting
-        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
-        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-       // left =  endtimes;
-        // right = indexofmeeting // sr no.
-
         ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
         for(int i=0; i<calendar.size() ; i++){
-            endTimes.add(Pair.of(calendar.get(i).getEndTime() , i));
+            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
         }
+
         Collections.sort(endTimes);
+
         LocalTime time_limit = endTimes.get(0).getLeft();
 
         int cnt = 0;
         if(!endTimes.isEmpty()){
             cnt += 1;
         }
-        for(int i=1;i < endTimes.size();i++){
+        for(int i = 1;i < endTimes.size();i++){
 
-            if(calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0){
+            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0){
                 cnt += 1;
                 time_limit = endTimes.get(i).getLeft();
             }
@@ -52,3 +47,10 @@ public class Workspace extends Gmail{
         return cnt;
     }
 }
+// find the maximum number of meetings you can attend
+// 1. At a particular time, you can be present in at most one meeting
+// 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
+// Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+// left =  endtimes;
+// right = indexofmeeting // sr no.
+
